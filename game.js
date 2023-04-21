47f5284aae0b562fb4e31cd8a4d8386911229186
Raulito01 <131450698+Raulito01@users.noreply.github.com>
2023-04-21 12:23:27 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<jnlp spec="1.0+" href="game.jnlp">
+  <information>
+    <title>R-Type Underwater Prototype</title>
+    <vendor>AgentGPT</vendor>
+    <homepage href=""/>
+    <description>A prototype of R-Type set underwater with a submarine.</description>
+    <description kind="short">R-Type Underwater Prototype</description>
+    <offline-allowed/>
+  </information>
+  <resources>
+    <j2se version="1.8+" />
+    <jar href="game.jar" main="true" />
+  </resources>
+  <application-desc main-class="Game" />
+</jnlp>
